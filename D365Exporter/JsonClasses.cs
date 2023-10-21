6f6c4ef0b89cee9d1ae287bb2a7754ab30d7653d
rreams <rreams@stigov.com>
2023-10-20 17:35:59 -0700

    base work

---
+++ D365Exporter/JsonClasses.cs
@@ -0,0 +1,115 @@
+﻿using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace D365Exporter;
+
+// using System.Xml.Serialization;
+// XmlSerializer serializer = new XmlSerializer(typeof(Trace));
+// using (StringReader reader = new StringReader(xml))
+// {
+//    var test = (Trace)serializer.Deserialize(reader);
+// }
+
+[XmlRoot(ElementName = "TraceCriteria")]
+public class TraceCriteria
+{
+
+    [XmlAttribute(AttributeName = "Direction")]
+    public string Direction { get; set; }
+
+    [XmlAttribute(AttributeName = "ItemId")]
+    public string ItemId { get; set; }
+
+    [XmlAttribute(AttributeName = "inventBatchId")]
+    public int InventBatchId { get; set; }
+}
+
+[XmlRoot(ElementName = "CurrentOnHand")]
+public class CurrentOnHand
+{
+
+    [XmlAttribute(AttributeName = "Qty")]
+    public double Qty { get; set; }
+
+    [XmlAttribute(AttributeName = "UnitId")]
+    public string UnitId { get; set; }
+}
+
+[XmlRoot(ElementName = "ItemNode")]
+public class ItemNode
+{
+
+    [XmlElement(ElementName = "ItemNode")]
+    public List<ItemNode> ChildItemNode { get; set; }
+
+    [XmlAttribute(AttributeName = "Item")]
+    public string Item { get; set; }
+
+    [XmlAttribute(AttributeName = "Category")]
+    public string Category { get; set; }
+
+    [XmlAttribute(AttributeName = "Reference")]
+    public string Reference { get; set; }
+
+    [XmlAttribute(AttributeName = "Qty")]
+    public double Qty { get; set; }
+
+    [XmlAttribute(AttributeName = "UnitId")]
+    public string UnitId { get; set; }
+
+    [XmlAttribute(AttributeName = "Date")]
+    public DateTime Date { get; set; }
+
+    [XmlAttribute(AttributeName = "InventSizeId")]
+    public string InventSizeId { get; set; }
+
+    [XmlAttribute(AttributeName = "InventStyleId")]
+    public string InventStyleId { get; set; }
+
+    [XmlAttribute(AttributeName = "InventSiteId")]
+    public string InventSiteId { get; set; }
+
+    [XmlAttribute(AttributeName = "InventLocationId")]
+    public string InventLocationId { get; set; }
+
+    [XmlAttribute(AttributeName = "wMSLocationId")]
+    public string WMSLocationId { get; set; }
+
+    [XmlAttribute(AttributeName = "InventStatusId")]
+    public string InventStatusId { get; set; }
+
+    [XmlAttribute(AttributeName = "inventBatchId")]
+    public int InventBatchId { get; set; }
+
+    [XmlAttribute(AttributeName = "AlreadyIncluded")]
+    public int AlreadyIncluded { get; set; }
+
+    [XmlAttribute(AttributeName = "PickedOnly")]
+    public int PickedOnly { get; set; }
+
+    [XmlAttribute(AttributeName = "InventColorId")]
+    public int InventColorId { get; set; }
+}
+
+[XmlRoot(ElementName = "TraceHierarchy")]
+public class TraceHierarchy
+{
+
+    [XmlElement(ElementName = "ItemNode")]
+    public List<ItemNode> ItemNode { get; set; }
+}
+
+[XmlRoot(ElementName = "Trace")]
+public class Trace
+{
+
+    [XmlElement(ElementName = "TraceCriteria")]
+    public TraceCriteria TraceCriteria { get; set; }
+
+    [XmlElement(ElementName = "CurrentOnHand")]
+    public CurrentOnHand CurrentOnHand { get; set; }
+
+    [XmlElement(ElementName = "TraceHierarchy")]
+    public TraceHierarchy TraceHierarchy { get; set; }
+}
+
---
+++ D365XMLExplorer/JsonClasses.cs
@@ -0,0 +1,98 @@
+﻿using System.Text.Json.Serialization;
+
+namespace D365XMLExplorer;
+public class CurrentOnHand
+{
+    [JsonPropertyName("_Qty")]
+    public string Qty { get; set; }
+
+    [JsonPropertyName("_UnitId")]
+    public string UnitId { get; set; }
+}
+
+public class ItemNode
+{
+    [JsonPropertyName("ItemNode")]
+    public List<ItemNode> ItemNodes { get; set; }
+
+    [JsonPropertyName("_Item")]
+    public string Item { get; set; }
+
+    [JsonPropertyName("_Category")]
+    public string Category { get; set; }
+
+    [JsonPropertyName("_Reference")]
+    public string Reference { get; set; }
+
+    [JsonPropertyName("_Qty")]
+    public string Qty { get; set; }
+
+    [JsonPropertyName("_UnitId")]
+    public string UnitId { get; set; }
+
+    [JsonPropertyName("_Date")]
+    public string Date { get; set; }
+
+    [JsonPropertyName("_InventSizeId")]
+    public string InventSizeId { get; set; }
+
+    [JsonPropertyName("_InventStyleId")]
+    public string InventStyleId { get; set; }
+
+    [JsonPropertyName("_InventSiteId")]
+    public string InventSiteId { get; set; }
+
+    [JsonPropertyName("_InventLocationId")]
+    public string InventLocationId { get; set; }
+
+    [JsonPropertyName("_wMSLocationId")]
+    public string WMSLocationId { get; set; }
+
+    [JsonPropertyName("_InventStatusId")]
+    public string InventStatusId { get; set; }
+
+    [JsonPropertyName("_inventBatchId")]
+    public string InventBatchId { get; set; }
+
+    [JsonPropertyName("_PickedOnly")]
+    public string PickedOnly { get; set; }
+
+    [JsonPropertyName("_AlreadyIncluded")]
+    public string AlreadyIncluded { get; set; }
+}
+
+public class Base
+{
+    [JsonPropertyName("Trace")]
+    public Trace Trace { get; set; }
+}
+
+public class Trace
+{
+    [JsonPropertyName("TraceCriteria")]
+    public TraceCriteria TraceCriteria { get; set; }
+
+    [JsonPropertyName("CurrentOnHand")]
+    public CurrentOnHand CurrentOnHand { get; set; }
+
+    [JsonPropertyName("TraceHierarchy")]
+    public TraceHierarchy TraceHierarchy { get; set; }
+}
+
+public class TraceCriteria
+{
+    [JsonPropertyName("_Direction")]
+    public string Direction { get; set; }
+
+    [JsonPropertyName("_ItemId")]
+    public string ItemId { get; set; }
+
+    [JsonPropertyName("_inventBatchId")]
+    public string InventBatchId { get; set; }
+}
+
+public class TraceHierarchy
+{
+    [JsonPropertyName("ItemNode")]
+    public List<ItemNode> ItemNode { get; set; }
+}
